e97efc372f917def4f710b79056cb3116e4fe928
Dennis <98999569+dennis112999@users.noreply.github.com>
2025-02-03 02:38:56 +0900

    [UIMaterialModifier] Add Get/Set Value API

--- Assets/UIMaterialModifier/Scripts/UI/UIMaterialModifier.cs
+++ Assets/UIMaterialModifier/Scripts/UI/UIMaterialModifier.cs
@@ -122,5 +122,113 @@ namespace Dennis.UI
             return modifiedMaterial;
         }
 
+        public void SetFloat(string propertyName, float value)
+        {
+            SetProperty(propertyName, value);
+        }
+
+        public void SetInt(string propertyName, int value)
+        {
+            SetProperty(propertyName, value);
+        }
+
+        public void SetColor(string propertyName, Color color)
+        {
+            SetProperty(propertyName, color);
+        }
+
+        public void SetVector4(string propertyName, Vector4 vector)
+        {
+            SetProperty(propertyName, vector);
+        }
+
+        public float GetFloat(string propertyName)
+        {
+            return GetProperty<float>(propertyName, 0f);
+        }
+
+        public int GetInt(string propertyName)
+        {
+            return GetProperty<int>(propertyName, 0);
+        }
+
+        public Color GetColor(string propertyName)
+        {
+            return GetProperty<Color>(propertyName, Color.white);
+        }
+
+        public Vector4 GetVector4(string propertyName)
+        {
+            return GetProperty<Vector4>(propertyName, Vector4.zero);
+        }
+
+        private void SetProperty<T>(string propertyName, T value)
+        {
+            if (Graphic == null || Graphic.material == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError($"[UIMaterialModifier] No valid material found on {gameObject.name}");
+#endif
+                return;
+            }
+
+            Material mat = Graphic.material;
+
+            if (!mat.HasProperty(propertyName))
+            {
+#if UNITY_EDITOR
+                Debug.LogError($"[UIMaterialModifier] Shader does not contain property '{propertyName}' on {gameObject.name}");
+#endif
+                return;
+            }
+
+            switch (value)
+            {
+                case float floatValue:
+                    mat.SetFloat(propertyName, floatValue);
+                    break;
+                case int intValue:
+                    mat.SetInt(propertyName, intValue);
+                    break;
+                case Color colorValue:
+                    mat.SetColor(propertyName, colorValue);
+                    break;
+                case Vector4 vectorValue:
+                    mat.SetVector(propertyName, vectorValue);
+                    break;
+            }
+
+            Graphic.SetMaterialDirty();
+        }
+
+        private T GetProperty<T>(string propertyName, T defaultValue)
+        {
+            if (Graphic == null || Graphic.material == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError($"[UIMaterialModifier] No valid material found on {gameObject.name}");
+#endif
+                return defaultValue;
+            }
+
+            Material mat = Graphic.material;
+
+            if (!mat.HasProperty(propertyName))
+            {
+#if UNITY_EDITOR
+                Debug.LogError($"[UIMaterialModifier] Shader does not contain property '{propertyName}' on {gameObject.name}");
+#endif
+                return defaultValue;
+            }
+
+            return propertyName switch
+            {
+                _ when typeof(T) == typeof(float) => (T)(object)mat.GetFloat(propertyName),
+                _ when typeof(T) == typeof(int) => (T)(object)mat.GetInt(propertyName),
+                _ when typeof(T) == typeof(Color) => (T)(object)mat.GetColor(propertyName),
+                _ when typeof(T) == typeof(Vector4) => (T)(object)mat.GetVector(propertyName),
+                _ => defaultValue
+            };
+        }
     }
 }
